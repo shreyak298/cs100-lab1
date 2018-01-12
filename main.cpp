81b21aa6ee2b9259210dedcf3f927864d19c8802
Shreya Kumar <skuma021@ucr.edu>
2018-01-11 16:54:39 -0800

    solved merge conflict between userinput and master branches

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 int main(){
-<<<<<<< HEAD
+
 	std::cout << "hello git" << std::endl;
 	return 0; 
-=======
+
 	std::string name;
 	std::cout << "What is your name?" << std::endl;
 	std::cin >> name;
 	std::cout << "Hello " << name << "!" << std::endl;
 
 	return 0;
->>>>>>> userinput
+
 }
 
